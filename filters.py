663dc107c751e4592d3672440ff94e5b19b84ad3
Rafael Maldonado <64608688+maldonraf@users.noreply.github.com>
2025-05-06 00:45:55 -0700

    Added filters module (incl. raised cosine filter); subplot support; bugfixes.

---
+++ filters.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def raised_cosine(beta, samples_per_symbol, symbol_cnt):
+    samples = symbol_cnt * samples_per_symbol
+    t = np.arange(-(samples//2), (samples//2) + 1) / samples_per_symbol
+
+    h = np.zeros_like(t)
+    for i, val in enumerate(t):
+        if val == 0.0:
+            h[i] = 1.0
+        elif beta != 0 and np.isclose(abs(val), 1/(2 * beta)):
+            h[i] = (np.pi / 4) * np.sinc(1 / (2 * beta))
+        else:
+            h[i] = (np.sinc(val) * np.cos(np.pi * beta * val)) / (1 - (2 * beta * val) ** 2)
+
+    h = h / np.sqrt(np.sum(h ** 2))
+    return h
--- sim_eye.py
+++ sim_eye.py
@@ -2,37 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import stream_gen as stream_gen
 import scipy
+import filters
 
 # ===== User Params =====
-bit_rate        = 1e3
-sample_rate     = 1e5
-signal_length   = 10000
+bit_rate        = (53.125e9 * 2)
+sample_rate     = bit_rate * 50
+signal_length   = 1000
+plot_symbol_cnt = 2
 # =====
+
+figure, axis = plt.subplots(2, 1)
 
 samples_per_symbol  = int(sample_rate / bit_rate)
 samples             = signal_length * samples_per_symbol
-t                   = np.linspace(0, signal_length/bit_rate, samples, endpoint=False)
-bit_stream          = stream_gen.stream_gen(length=signal_length, levels=4)
-symbol_stream       = 2 * bit_stream - 1
-signal              = np.repeat(symbol_stream, samples_per_symbol)
+symbol_stream       = stream_gen.stream_gen(length=signal_length, levels=4) - 1.5
+signal = []
+for symbol in symbol_stream:
+    pulse = np.zeros(samples_per_symbol)
+    pulse[0] = symbol
+    signal = np.concatenate((signal, pulse))
 
-wn      = 2 * np.pi * 1e2
-zeta    = 0.7
-system  = scipy.signal.lti([wn**2], [1, 2 * zeta * wn, wn**2])
-tout, h = scipy.signal.impulse(system)
+h = filters.raised_cosine(0.8, samples_per_symbol, 100)
+
+axis[0].set_title('Filter Impulse Responses')
+axis[0].plot(h)
 
 tx_signal   = np.convolve(signal, h, mode='same')
-t           = np.arange(len(tx_signal)) / sample_rate
 
-plt.figure(figsize=(10,4))
-
-for i in range(len(tx_signal)//5000):
-    plot_range = (len(tx_signal)//5000)
+axis[1].set_title('Eye Capture')
+plot_cnt = len(signal)//(plot_symbol_cnt * samples_per_symbol)
+for i in range(plot_cnt-1):
+    plot_range = plot_symbol_cnt * samples_per_symbol
     subrange_start = i * plot_range
     subrange_end = subrange_start + plot_range
-    plt.plot(tx_signal[subrange_start:subrange_end])
+    axis[1].plot(tx_signal[subrange_start:subrange_end])
 
-plt.xlabel('Time (s)')
-plt.ylabel('Amplitude')
-plt.grid(True)
-plt.show()
+figure.show()
